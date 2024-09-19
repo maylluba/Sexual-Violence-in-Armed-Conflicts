93c39028ec73e902e0712336322297a0a9e8c8d9
maylluba <lluesma.etyam@gmail.com>
2024-09-19 12:03:43 +0200

    sklearn added

--- SVAC.py
+++ SVAC.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
